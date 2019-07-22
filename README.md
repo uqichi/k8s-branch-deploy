4539f4f19ec348b1ab3e3d2d32c5088e35e89fda
Tamura Yukichi <tamura.yukichi@gmail.com>
2019-07-22 20:05:41 +0900

    Add subdomain field

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-# nginx-go-api
-small sample app running as container
+# sample-k8s
+small sample app, which consists of some containers, for testing kubernetes (EKS).
+
+Start in localhost:
 
 ```shell
 make up
 ```
 
-```shell
-open http://localhost:8080
+Append the following line to `/etc/hosts`:
+
 ```
+127.0.0.1 apple.hoge-dev.myproj.com
+```
+
+```shell
+open http://apple.hoge-dev.myproj.com:8080
+```
+
+Check the console log on browser and server log.
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -21,6 +24,24 @@ func main() {
 		//AllowCredentials: true,
 	}))
 
+	// Set sub domain to context
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) (err error) {
+			req := c.Request()
+			fmt.Println("req.Host", req.Host)
+			host := strings.Split(req.Host, ":")[0]
+			args := strings.Split(host, ".")
+			fmt.Println("host", host)
+			sub0 := args[0]
+			fmt.Println("sub0", sub0)
+			if sub0 == "" {
+				return echo.ErrBadRequest
+			}
+			setCtxSub0(c, sub0)
+			return next(c)
+		}
+	})
+
 	// Routes
 	e.GET("/ping", ping)
 
@@ -29,9 +50,27 @@ func main() {
 }
 
 func ping(c echo.Context) error {
+	fmt.Println("pong!")
+	fmt.Println(getCtxSub0(c))
 	return c.JSON(http.StatusOK, struct {
-		Value string `json:"value"`
+		Value     string `json:"value"`
+		SubDomain string `json:"sub_domain"`
 	}{
-		Value: "pong!!",
+		Value:     "pong!!",
+		SubDomain: getCtxSub0(c),
 	})
+}
+
+func setCtxSub0(ctx echo.Context, s string) {
+	ctx.Set("ieBee3qu", s)
+}
+
+func getCtxSub0(ctx echo.Context) string {
+	val := ctx.Get("ieBee3qu")
+	ret, ok := val.(string)
+	if !ok {
+		log.Println("fail getCtxSub0")
+		return ""
+	}
+	return ret
 }
